24821dbf265d67bb67f6f219732c9d909d28be2c
bradyJ89 <omfguess@aim.com>
2013-08-31 16:07:47 -0400

    alternate coverflow version
    attempting to create the effect from scratch

--- js/swipe.js
+++ js/swipe.js
@@ -1,7 +1,7 @@
 document.write('<style type="text/css">body{display:none}</style>');//hide content while jquery sets css
 $(document).ready(function(){
 	var index = 0,
-		articles = new Array(),
+		articles = [],
 		width = $(document).width(),
 		height = $(document).height(),
 		left = -width + 'px',
@@ -20,12 +20,18 @@ $(document).ready(function(){
 		"<p class='articleText'>article5 <p>article5 article5 article5 article5 article5<p> article5 article5 article5 article5 article5 article5 article5 article5<p>",
 		"<h1><a href=''target=''>Giant microwave blaster lets bread stay fresh-ish for over 60 days</a></h1><h2><a href='https://dvice.com/' target='_blank'>DVICE</a> November 30th, 2012 16:46 EST</h2><div id='szzzl-story-long-wrapper'><p><div><div><p class='articleText'>About a third of the bread that consumers buy gets tossed out or fed to ducks who don't know any better after it goes stale or gets moldy. You just cant win with bread- either you keep it moist and gross stuff grows on it, or you dry it out and it turns into a rock. A Texas company says it has a solution, in the form of a huge homogenized microwave cannon.</p><p class='articleText'>Microwaves, being radiation, work quite well at killing things like fungi. The reason that you cant use your microwave to kill fungi is that your microwave sucks, and due to the wavelength of the microwaves used in microwave ovens (just under five inches), you get hot spots and cold spots that show up at half of that wavelength. Microzap's microwave chamber, on the other hand, works differently, and much better. CEO Don Stull explains:</p><div>For the latest tech stories, follow DVICE on Twitter at @dvice or find us on Facebook</div></div></div></p><p><a href='https://dvice.com/archives/2012/11/giant-microwave.php' target='_blank'>Read more</a></p></div>"
 	);
-	
+	//could put this in self-executing function
+	startSize = $(window).width();
 	$(window).on('resize load', function(){//adjust elements for different screen sizes
 		//alert(window.devicePixelRatio);
 		$('body').show();
+		var difference = Math.abs($(window).width() - startSize);
+
+		var adjust = difference / startSize * $(window).width();
+		console.log(adjust);
+		//console.log(difference);
 		var w_box = $(window).width(),
-			h_box = $('#stream').height(),	
+			h_box = $('#stream').height(),
 			w_total = ((w_box - $('.article').width())/2) - 2, //400
 			h_total = (h_box - $('.article').height())/2,
 			css = {"position": 'absolute',"margin-left": w_total +"px"};
@@ -33,9 +39,13 @@ $(document).ready(function(){
 		$('#paper0').css('margin-left', w_total + 2 + 'px');
 		$('#paper1').css('margin-left', w_total + 4 + 'px');
 		$('#paper2').css('margin-left', w_total + 6 + 'px');
-		$('#username').css('margin-left', parseInt($('.article').css('margin-left')) + 'px');
+		$('#username').css('margin-left', parseInt($('.article').css('margin-left'), 10) + 'px');
 		$('#share p').css('margin-right', $('#topArticle').css('margin-left'));
 		$('#shareMenu').css('margin-left', '-' + ($('#share p').css('margin-right')));
+		$('.szld').css('width', '10%');
+		$('.szld:last').prevAll().each(function(){
+			$(this).css('left', parseInt($(this).css('left'), 10) - 0.10 + 'px');
+		});
 		artPos = $('#topArticle').css('margin-left');
 		artLeft = $('#topArticle').css('left');
 		width = $(window).width();
@@ -50,20 +60,21 @@ $(document).ready(function(){
 
 	$('#topArticle').html(articles[5]);
 	$('#queue').on('click','div', function(){//switch top article content with queued content
-		duplicate = true;
+		//duplicate = true;
+		var closestLeft = $(this).next().attr('id');//article to the left of the clicked one
+		$(this).remove();
+		$('#' + closestLeft).prevAll().each(function(){
+			$(this).animate({left: parseInt($(this).css('left'), 10) - ($(window).width() * 0.10) + 'px'},{duration: 500});
+		});
 		$('#queue div').removeClass('rerate');
 		$(this).addClass('rerate');
 		var contCopy = $(this).contents().clone();
-		$('#holder').append(contCopy);
+		//$('#holder').append(contCopy);
 		$('#middleArticle').addClass('requeue').empty().append($('#topArticle').contents());
-		$('#topArticle').addClass('rerate').empty().append($('#holder').contents());
-		$('#holder').empty();
+		$('#topArticle').addClass('rerate').empty().append(contCopy);
+		//$('#holder').empty();
 	});
-
-	$(document).click(function(e){
-		console.log('click')
-				console.log($(event.target).attr('id'));
-			});
+	var dragged = false;
 	szl = function(e){
 		//console.log(duplicate);
 		if (!isRunning){
@@ -79,37 +90,48 @@ $(document).ready(function(){
 				}
 			}).css({'box-shadow': '0 0 .5em red'});
 			createArticle();
-			$('#queue .szld').each(function(){//loop through szld list
+			/*$('#queue .szld').each(function(){//loop through szld list
 				if ($(this).text() == artText) {//compare text inside topArticle and queue div
 					//////console.log('match');
 					duplicate = true;
 				}
-			});
+			});*/
 
 			if (duplicate){//if article already in queue, don't add it
 				duplicate = false;//next article will be different, so this allows the else block to run
 			} else {
 				////console.log('add')//if not in queue, add it
+			$('#queue').scrollLeft(0);
 				$('#queue div').each(function(){
-					$(this).animate({left: parseInt($(this).css('left')) + ($(window).width() * .15) + 'px'},{duration: 500});
+					$(this).animate({left: parseInt($(this).css('left'), 10) + ($(window).width() * 0.10) + 'px'},{duration: 500});
 				});
-			var newDiv = document.createElement('div');
-			$('#queue').append($(newDiv).attr('id', 'newSzl' + szlCount).css({
-				'position':'absolute', 'width':'20%', 'height':'100%',
-				//'left': -($(document).width()),
-				'background':'white',
-				//'transition':'1s', 'overflow':'hidden',
-				'box-shadow':'0 0 .5em black',
-				'z-index': szlCount,
-				//'transform':'rotateY(-45deg)',
-				'left': 0
-			}).addClass('szld').css('overflow','hidden').append(szld));
-			szlCount += 1;
-			//duplicate = false;
+				var newDiv = document.createElement('div');
+				$('#queue').append($(newDiv).attr('id', 'newSzl' + szlCount).css({
+					'position':'absolute', 'width':'20%', 'height': '90%',
+					'background':'white',
+					'box-shadow':'0 0 .5em black',
+					'z-index': szlCount + 1,
+					'top':'10%',
+					'left': 0
+				}).addClass('szld').css('overflow','hidden').append(szld).scrollLeft(0));
+				console.log($('.szld:last').attr('id'));
+				$('.szld:last').prevAll().each(function(index){
+					$(this).css('top', parseInt($(this).next().css('top'), 10) + (parseInt($(this).next().css('top'), 10) * 0.15) + 'px');
+				});
+				/*if (dragged) {
+				$('#queue').animate({
+					left: 0
+				}, 500).find('div').css({'box-shadow':'0 0 .8em black', 'z-index':'100'}).end()
+					.find('.szld').each(function(){
+						$(this).css('z-index', $(this).prev().css('z-index') + 1);
+					});
+				}*/
+				$('#queue, footer').scrollLeft(0);
+				$('#queue').animate({left: 0}, 500);
+				szlCount += 1;
 			}
 		}
-	}
-
+	};
 	fzl = function(e){
 		if (!isRunning){
 			isRunning = true;
@@ -118,10 +140,10 @@ $(document).ready(function(){
 					var rerateID = $('#queue .rerate').next('.szld').attr('id');
 					$('#queue .rerate').remove();
 					$('.szld').each(function(){
-					   	var current = $(this).index();
-					   	if(current < $("#" + rerateID).index() || rerateID == undefined) {
-					     	$(this).animate({left: parseInt($(this).css('left')) - ($(window).width() * .15) + 'px'},{duration: 500});
-					   	}
+						var current = $(this).index();
+						if(current < $("#" + rerateID).index() || rerateID === undefined) {
+							//$(this).animate({left: parseInt($(this).css('left')) - ($(window).width() * .15) + 'px'},{duration: 500});
+						}
 					});
 				}
 				$(this).remove();
@@ -132,16 +154,16 @@ $(document).ready(function(){
 			}).css({'box-shadow': '0 0 .5em blue'});
 			createArticle();
 		}
-	}	
+	};
 
 	//share menu
 	$('#shareText').click(function(){
 		////console.log($('#shareMenu').css("height"));
-		if (parseInt($('#shareMenu').css('height')) > 0){
+		if (parseInt($('#shareMenu').css('height'), 10) > 0){
 			$('#shareMenu').animate({height: 0},500);
 		}
 		else {
-			$('#shareMenu').css("left", ($(window).width() - parseInt($('#shareMenu').css('width')) + 'px'))
+			$('#shareMenu').css("left", ($(window).width() - parseInt($('#shareMenu').css('width'), 10) + 'px'));
 			$('#shareMenu').animate({height: '938px'}, 500);
 		}
 	});
@@ -152,7 +174,7 @@ $(document).ready(function(){
 		$('#stream').append(newCont);
 		//$('#bottomArticle').addClass('article').html(articles[index]).css('margin-left', artPos);
 		if ($('#middleArticle').hasClass('requeue')){
-			if(index == 0){
+			if(index === 0){
 				//console.log('its 0');
 				$('#bottomArticle').addClass('article').html(articles[index + 5]).css('margin-left', artPos);
 			} else {
@@ -164,7 +186,7 @@ $(document).ready(function(){
 			index += 1;
 		}
 		if (index > 5){ index = 0;}
-	}	
+	};
 
 	//var initial = parseInt($('#topArticle').css('left'));
 	//var firstDrag = true;
@@ -173,28 +195,29 @@ $(document).ready(function(){
 		//start: function(ui, event){},
 		//drag: function(ui,event){},
 		//stop: function(ui, event){}
-	}
+	};
 	$('.article').draggable(newArticle);
 
 	////
-	var start_time;
+	var start_time,
 		startPos = {x:0, y:0},
 		distance = {x:0, y:0},
-		scaledDistance = 0,
-		swipeTime = 0,
-		angle = 0;
+		scaledDistance, swipeTime, swipeAngle = 0;
+		//swipeTime = 0,
+		//swipeAngle = 0;
+
 	function start(e) {
-	    start_time = new Date();
+		start_time = new Date();
 		startPos.x = e.pageX;
 		startPos.y = e.pageY;
 	}
+
 	function end(e) {
-	    var now = new Date();
-	    swipeTime = (now - start_time)/1000;
+		var now = new Date();
+		swipeTime = (now - start_time)/1000;
 		distance.x = e.pageX - startPos.x;
 		distance.y = e.pageY - startPos.y;
-		angle = Math.atan(distance.y / distance.x) * (180 / Math.PI);
-
+		swipeAngle = Math.atan(distance.y / distance.x) * (180 / Math.PI);
 		scaledDistance = Math.sqrt((distance.x / width)^2 + (distance.y / height)^2);
 	}
 
@@ -203,10 +226,10 @@ $(document).ready(function(){
 	}).mouseup(function(e){
 		end(e);
 		if (!isRunning) checkForSwipe();
-	});	
+	});
 
 	checkForSwipe = function(){
-		if (Math.abs(angle) <= 45){
+		if (Math.abs(swipeAngle) <= 45){
 			if (scaledDistance / swipeTime > 3){
 				if (distance.x > 0){
 					szl();
@@ -233,23 +256,98 @@ $(document).ready(function(){
 		else {
 			$('#topArticle').animate({left: 0 },300);
 		}
-	}
+	};
 
-	var orig;
 	$('#queue').on('mouseenter', 'div', function(event){
-		orig = $(this).css('z-index');
-		$(this).css('z-index', '100');//.css('transform','rotateY(0deg)').css('z-index', '100');
+		//orig = $(this).css('z-index');
+		//$(this).css('z-index', '100');//.css('transform','rotateY(0deg)').css('z-index', '100');
 	}).on('mouseleave', 'div', function(event){
-		$(this).css('z-index', orig);//.css('transform','rotateY(-45deg)')
+		//$(this).css('z-index', orig);//.css('transform','rotateY(-45deg)')
 	});
+
+	var zCount = {left: 1000, right: 1000};
+	var orig;
+	$('#queue').draggable({
+		axis: "x",
+		scroll: false,
+
+		start: function(){
+			dragged = true;
+		},
+
+		drag: function(){
+			/*if ($('.szld:last').offset().left > $(window).width() - ($(window).width() * .10)){
+			//return false;
+			}*/
+			$('.szld').each(function(){
+				var szldItem = $(this).offset().left / $(window).width() * 100;
+				if (szldItem < 38) {
+					$(this).css({//adjust z-index for elements left of center & set top
+						"z-index": $(this).prev().css("z-index") - 1, "top": 55 - szldItem + '%'
+					});//.css('width', 60 - szldItem + '%');
+				} else if (szldItem > 38) {
+					//console.log($(this).offset().left / $(window).width() * 100);
+					$(this).css('top', -35 + szldItem + '%');
+					//console.log('> 39: ', $(this).css('z-index'));
+				}
+
+				//set border shadow for central item
+				if (szldItem > 38 &&  szldItem < 45) {
+					console.log('true');
+					console.log($(this).offset().left / $(window).width() * 100);
+					//console.log($(this).css('z-index'));
+					$(this).css({'z-index' : '200000' , 'box-shadow':'0 0 1em red'});
+				}
+
+				//set shadow for peripheral items
+				if (szldItem > 45 & szldItem < 55) {
+					$(this).css({'z-index' : zCount.right, 'box-shadow':'0 0 .8em black'});
+					zCount.right = zCount.right + 1;
+				} else if (szldItem < 38 & szldItem > 35) {
+					$(this).css({'z-index' : zCount.left, 'box-shadow':'0 0 .8em black'});
+					zCount.left = zCount.left + 1;
+				}
+
+				/*if (szldItem > -6 & szldItem < 5) {
+					$(this).css({'z-index' : '1', 'box-shadow':'0 0 .8em black'});
+				}
+				if (szldItem > 5 & szldItem < 15) {
+					$(this).css({'z-index' : '2', 'box-shadow':'0 0 .8em black'});
+				}
+				if (szldItem > 15 & szldItem < 25) {
+					$(this).css({'z-index' : '3', 'box-shadow':'0 0 .8em black'});
+				}
+				if (szldItem > 25 & szldItem < 35) {
+					$(this).css({'z-index' : '4', 'box-shadow':'0 0 .8em black'});
+				}
+				if (szldItem > 35 & szldItem < 45) {
+					$(this).css({'z-index' : '5', 'box-shadow':'0 0 .8em black'});
+				}*/
+				/*if (szldItem < 50){
+					$(this).css('top', parseInt($(this).next().css('top')) + (parseInt($(this).next().css('top')) * szldItem) + 'px');
+				}
+				if (szldItem > 49 && szldItem < 51){
+					console.log('true');
+					$(this).css('top', '10%').css('box-shadow','0 0 .8em red').css('z-index','100').nextAll().each(function(){
+					$(this).css('top', parseInt($(this).prev().css('top')) + (parseInt($(this).prev().css('top')) * szldItem / 100) + 'px');
+				}).end().prevAll().each(function(){
+					$(this).css('top', parseInt($(this).next().css('top')) + (parseInt($(this).next().css('top')) * szldItem / 100) + 'px');
+				});
+				} else {
+					$(this).css('box-shadow','0 0 .5em black');
+				}*/
+			});
+		}
+	});
+
 
 	$('#stream').click(function(e){
 		e.stopPropagation();
 		if (!isRunning){
-			if (e.pageX < $(document).width() * .10){//within 10% of left edge
+			if (e.pageX < $(document).width() * 0.10){//within 10% of left edge
 				console.log('fzl');
 				fzl();
-			} else if (e.pageX > $(document).width() - ($(document).width() * .10)){//right edge
+			} else if (e.pageX > $(document).width() - ($(document).width() * 0.10)){//right edge
 				console.log('szl');
 				szl();
 			}
@@ -257,7 +355,7 @@ $(document).ready(function(){
 	});
 
 	/////
-	var sidevalue = .10,
+	var sidevalue = 0.15,
 		arrows = true;
 	$('.arrows').mouseup(function(e){
 		if (e.pageX < $(document).width() * sidevalue){//within 10% of left edge
@@ -272,8 +370,8 @@ $(document).ready(function(){
 				$('.arrows').fadeIn();
 				arrows = false;
 				setTimeout(function(){
-	                arrows = true;
-	            }, 800);
+					arrows = true;
+				}, 800);
 			}
 		}
 	}).on('mouseup', function(){
