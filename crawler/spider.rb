a87d0168a5b4d7cee3c4d3e743fbd4dee270dc91
bradyJ89 <omfguess@aim.com>
2013-11-15 14:05:49 -0500

    updated list

--- crawler/spider.rb
+++ crawler/spider.rb
@@ -41,7 +41,37 @@ require 'open-uri'
 	mainURL = []
 	xml.search( '//item' ).each do |a|
 		%w[link title].each do |n|
-			puts a.at(n).text
+			mainURL.push( a.at(n).text )
 		end
 	end
 	puts mainURL
+
+#bloomberg
+	html = Nokogiri::HTML( open( "http://www.bloomberg.com/archive/news/" ) ) and nil
+	urls = []
+	html.xpath( '//ul[@class="stories"]/li/a' ).each do |a|
+		title_and_link = "http://www.bloomberg.com" + a["href"], a.text
+		urls.push [ title_and_link ]
+	end
+	puts urls
+
+#yahoo
+	html = Nokogiri::HTML( open( "http://news.yahoo.com/archive/" ) ) and nil
+	urls = []
+	html.xpath( '//div[@id="MediaStoryList"]//h4/a' ).each do |a|
+		#need to check for 'http'
+		#if some already have full link, don't need to create a custom string
+		unless a["href"].match(/http/)
+			title_and_link = "http://news.yahoo.com/archive/" + a["href"], a.text
+		else 
+			title_and_link = a["href"], a.text.gsub('[$$]', '') #some titles contain [$$] at the beginning
+		end
+		urls.push [ title_and_link ]
+	end
+	puts urls
+
+#the loop
+	html = Nokogiri::HTML( open( "http://www.loopinsight.com/2013/" ) )
+	html.xpath( '//div[@class="container"]//h2/a' ).each do |a|
+		puts a['href'], a.text
+	end
